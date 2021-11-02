5ddd4959b5d47f7f4f9cf85ef473e7c4a0b52e63
Steve KOUNA <steve.kouna@afrinic.net>
2021-11-02 11:41:55 +0400

    Class de configuration

--- src/main/java/com/koona/invoise/core/App.java
+++ src/main/java/com/koona/invoise/core/App.java
@@ -1,32 +1,47 @@
 package com.koona.invoise.core;
 
 import com.koona.invoise.controller.InvoiceControllerInterface;
-import com.koona.invoise.controller.scan.InvoiceDouchetteController;
-import com.koona.invoise.controller.keyboard.InvoiceKeyBoardController;
-import com.koona.invoise.controller.web.InvoiceWebController;
-import com.koona.invoise.entity.Invoice;
-import com.koona.invoise.repository.database.InvoiceDatabaseRepository;
-import com.koona.invoise.repository.memory.InvoiceMemoryRepository;
-import com.koona.invoise.repository.InvoiceRepositoryInterface;
-import com.koona.invoise.service.prefix.InvoicePrefixService;
-import com.koona.invoise.service.number.InvoiceNumberService;
 import com.koona.invoise.service.InvoiceServiceInterface;
-import java.util.Scanner;
+import com.koona.invoise.service.prefix.InvoicePrefixService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
 
 /**
  * Hello world!
  *
  */
+@Configuration
+@ComponentScan(basePackages = {
+//    "com.koona.invoise.service.prefix",
+    "com.koona.invoise.controller.web",
+    "com.koona.invoise.repository.database"
+})
+//@PropertySource("classpath:application.properties")
 public class App 
 {
     public static void main( String[] args )
     {   
 //        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
         InvoiceControllerInterface invoiceControllerInterface = context.getBean(InvoiceControllerInterface.class);
         invoiceControllerInterface.createInvoice();
+    }
+    
+    @Bean
+    public InvoiceServiceInterface configInvoiceService() {
+        return new InvoicePrefixService();
+    }
+    
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+        configurer.setLocation(new ClassPathResource("application.properties"));
+        return configurer;
     }
 }
